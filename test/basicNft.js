30c36aa07299aa7b2cc5269ce76d28c07b428c2a
TEE2DWHY <isaacolorunfemi330@gmail.com>
2024-01-12 12:33:58 +0100

    added full functionality to basicNft.test

--- test/basicNft.js
+++ test/basicNft.js
@@ -1,42 +1,53 @@
 const { assert } = require("chai");
-const { ethers, waffle } = require("hardhat");
+const { ethers } = require("hardhat");
 const verifyContract = require("../utils/verify");
 
 describe("DogNft Deployment", () => {
-  let owner;
+  let deployer;
   let DogNft;
   let nft;
 
   beforeEach(async () => {
-    // Get the signers (accounts) from the hardhat network
-    [owner] = await ethers.getSigners();
-
-    // Deploy the DogNft contract
+    [deployer] = await ethers.getSigners();
     DogNft = await ethers.getContractFactory("DogNft");
-    // Deploy the contract
     nft = await DogNft.deploy();
-
-    // Wait for deployment
-    await nft.waitForDeployment(6);
-
-    // // Verify the contract on Etherscan
-    // await verifyContract(nft.target);
+    // await nft.waitForDeployment(6);
+    // Verify the contract on Etherscan
+    // Note: Uncomment the following line if you want to verify on Etherscan
+    // await verifyContract(nft.address);
   });
 
-  it("initializes the nft correctly.", async () => {
-    const name = await nft.name();
-    const symbol = await nft.symbol();
-    const tokenCounter = await nft.getTokenCounter();
-    assert.equal(name, "Gerald", "Name is Incorrect");
-    assert.equal(symbol, "GER", "Incorrect NFT Symbol");
-    assert.equal(tokenCounter, "0");
+  describe("Constructor", () => {
+    it("initializes the nft correctly.", async () => {
+      const name = await nft.name();
+      const symbol = await nft.symbol();
+      const tokenCounter = await nft.getTokenCounter();
+      assert.equal(name, "Gerald", "Name is Incorrect");
+      assert.equal(symbol, "GER", "Incorrect NFT Symbol");
+      assert.equal(tokenCounter, 0, "Token counter should be 0 initially");
+    });
   });
 
-  it("should check if the owner is set correctly", async () => {
-    assert.equal(
-      await nft.getOwner(),
-      owner.address,
-      "Owner is not set correctly"
-    );
+  describe("Mint NFT", () => {
+    beforeEach(async () => {
+      const transactionResponse = await nft.mintNFt();
+      await transactionResponse.wait(1);
+    });
+    it("Allows users to mint an NFT, and updates appropriately", async function () {
+      const tokenURI = await nft.tokenURI(0);
+      const tokenCounter = await nft.getTokenCounter();
+
+      assert.equal(tokenCounter.toString(), "1");
+      assert.equal(tokenURI, await nft.TOKEN_URI());
+    });
+
+    it("Show the correct balance and owner of an NFT", async function () {
+      const deployerAddress = deployer.address;
+      const deployerBalance = await nft.balanceOf(deployerAddress);
+      const owner = await nft.ownerOf("0");
+
+      assert.equal(deployerBalance.toString(), "1");
+      assert.equal(owner, deployerAddress);
+    });
   });
 });
