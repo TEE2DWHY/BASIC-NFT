047032c9daf61f5e46e3402b5b673d1d6128fe06
TEE2DWHY <isaacolorunfemi330@gmail.com>
2024-01-12 12:01:30 +0100

    added more test functionality to basicNft.test

--- test/basicNft.js
+++ test/basicNft.js
@@ -5,6 +5,7 @@ const verifyContract = require("../utils/verify");
 describe("DogNft Deployment", () => {
   let owner;
   let DogNft;
+  let nft;
 
   beforeEach(async () => {
     // Get the signers (accounts) from the hardhat network
@@ -12,24 +13,30 @@ describe("DogNft Deployment", () => {
 
     // Deploy the DogNft contract
     DogNft = await ethers.getContractFactory("DogNft");
-  });
-
-  it("should deploy DogNft contract and verify on Etherscan", async () => {
     // Deploy the contract
-    const nft = await DogNft.deploy();
+    nft = await DogNft.deploy();
 
     // Wait for deployment
     await nft.waitForDeployment(6);
 
-    // Verify the contract on Etherscan
-    await verifyContract(nft.target);
+    // // Verify the contract on Etherscan
+    // await verifyContract(nft.target);
+  });
 
-    // Check if the contract owner is set correctly or perform additional checks if needed
+  it("initializes the nft correctly.", async () => {
+    const name = await nft.name();
+    const symbol = await nft.symbol();
+    const tokenCounter = await nft.getTokenCounter();
+    assert.equal(name, "Gerald", "Name is Incorrect");
+    assert.equal(symbol, "GER", "Incorrect NFT Symbol");
+    assert.equal(tokenCounter, "0");
+  });
+
+  it("should check if the owner is set correctly", async () => {
     assert.equal(
       await nft.getOwner(),
       owner.address,
       "Owner is not set correctly"
     );
-    // Add more assertions or checks as needed
   });
 });
